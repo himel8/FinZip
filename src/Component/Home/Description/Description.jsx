cdb6f6ae94f8abc3193c515aaa5c070a82c53b3d
himel8 <himelkhan290@gmail.com>
2022-10-01 17:56:39 +0600

    reduce section margin

--- src/Component/Home/Description/Description.jsx
+++ src/Component/Home/Description/Description.jsx
@@ -1,41 +1,8 @@
 import React from "react";
-import Desc1 from "../../../assets/img/desc1.png";
-import Desc2 from "../../../assets/img/desc2.png";
-import Desc3 from "../../../assets/img/desc3.png";
+
 import Button from "../../shared/Button";
 import SubText from "../../shared/SubText";
-
-const descData = [
-  {
-    id: 1,
-    link: "#!",
-    img: Desc1,
-    title: "Finzip For Businesses",
-    muteText:
-      "Finzip provides the comprehensive coverage that businesses need to protect themselves from liability. Our team of experienced insurance professionals are available to help your business choose the right coverage for your needs and  budget.",
-    paraText:
-      "Contact us today to learn more about our business insurance solutions.",
-  },
-  {
-    id: 2,
-    link: "#!",
-    img: Desc2,
-    title: "Employee Health Insurance",
-    muteText:
-      "At Finzip, we understand that your employee are your most valuable asset. We provide comprehehensive employee health insurance plans to keep them healthy and productive,",
-    paraText:
-      "Be assured of assisted cashiess claims experience and give you emloyees the peace of mind they deserve.",
-  },
-  {
-    id: 3,
-    link: "#!",
-    img: Desc3,
-    title: "Mutual Fund Investing Platform",
-    muteText:
-      "Our platform fiexibility to invest in smaller amounts, making it an ideal choice for those who want to make investments in smaller amounts periodically,",
-    paraText: "Looking to invest in mutual fund?",
-  },
-];
+import { descData } from "./descriptionData";
 
 const Description = () => {
   return (
@@ -43,9 +10,9 @@ const Description = () => {
       {descData.map((data) => (
         <div
           key={data.id}
-          className={`flex justify-center items-center min-h-screen gap-4 flex-wrap lg:flex-nowrap my-10 mb-20 lg:mb-0 ${
-            data.id % 2 === 0 ? "flex-row-reverse" : "flex-row"
-          }`}
+          className={`flex justify-center items-center gap-4 flex-wrap lg:flex-nowrap ${
+            data.id === 1 ? "my-20 lg:mt-0 lg:mb-20" : "my-20"
+          }  ${data.id % 2 === 0 ? "flex-row-reverse" : "flex-row"}`}
         >
           <div className="w-full">
             <img src={data.img} alt="" className="w-full" />
---
+++ src/Component/Home/Description/descriptionData.js
@@ -0,0 +1,34 @@
+import Desc1 from "../../../assets/img/desc1.png";
+import Desc2 from "../../../assets/img/desc2.png";
+import Desc3 from "../../../assets/img/desc3.png";
+export const descData = [
+  {
+    id: 1,
+    link: "#!",
+    img: Desc1,
+    title: "Finzip For Businesses",
+    muteText:
+      "Finzip provides the comprehensive coverage that businesses need to protect themselves from liability. Our team of experienced insurance professionals are available to help your business choose the right coverage for your needs and  budget.",
+    paraText:
+      "Contact us today to learn more about our business insurance solutions.",
+  },
+  {
+    id: 2,
+    link: "#!",
+    img: Desc2,
+    title: "Employee Health Insurance",
+    muteText:
+      "At Finzip, we understand that your employee are your most valuable asset. We provide comprehehensive employee health insurance plans to keep them healthy and productive,",
+    paraText:
+      "Be assured of assisted cashiess claims experience and give you emloyees the peace of mind they deserve.",
+  },
+  {
+    id: 3,
+    link: "#!",
+    img: Desc3,
+    title: "Mutual Fund Investing Platform",
+    muteText:
+      "Our platform fiexibility to invest in smaller amounts, making it an ideal choice for those who want to make investments in smaller amounts periodically,",
+    paraText: "Looking to invest in mutual fund?",
+  },
+];
