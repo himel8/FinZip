b0730b285a5bcdb8def2ca95a6ef41d171540e9b
himel8 <himelkhan290@gmail.com>
2022-10-03 19:06:22 +0600

    c to arrow

--- src/Component/Home/HeroSection/HeroSection.jsx
+++ src/Component/Home/HeroSection/HeroSection.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import HeroBg from "../../../assets/img/hero-bg.png";
 import Button from "../../shared/Button";
 import SubText from "../../shared/SubText";
-import Title from "../../shared/Title";
 
 const HeroSection = () => {
   return (
     <section className="w-[95%] sm:w-[90%] md:w-[80%]  lg:w-[75%] mx-auto flex justify-center items-center min-h-screen flex-wrap-reverse lg:flex-nowrap gap-8 lg:gap-0 lg:mt-10 lg:my-0  my-20">
-      <div className="flex flex-col gap-6 relative lg:after:content-arrow after:absolute after:-top-2 after:left-1/2">
-        <Title first="Health" middle="Insurance." last="Simplified" />
+      <div className="flex flex-col gap-6 ">
+        <h2 className="text-[2.5rem] leading-[2.8rem] md:text-6xl font-normal font-title text-center lg:text-left  md:leading-[3.8rem]">
+          Health
+          <br />
+          Insuran
+          <span className="relative lg:after:content-arrow after:absolute after:-top-16 after:left-4">
+            c
+          </span>
+          e.
+          <br />
+          Simplified
+        </h2>
         <SubText Style="px-4 lg:px-0 pr-0 lg:pr-8 text-lg">
           Insurance is a vital part of any business.
           <br /> Get industry-focused insurance solutions that are best-suited
