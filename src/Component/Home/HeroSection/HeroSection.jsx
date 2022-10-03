7b3dbfead79c1350dccc515b8165586cf33c76fe
himel8 <himelkhan290@gmail.com>
2022-10-03 18:03:42 +0600

    reduce hero section

--- src/Component/Home/HeroSection/HeroSection.jsx
+++ src/Component/Home/HeroSection/HeroSection.jsx
@@ -6,10 +6,10 @@ import Title from "../../shared/Title";
 
 const HeroSection = () => {
   return (
-    <section className="w-[95%] sm:w-[90%] md:w-[80%] mx-auto flex justify-center items-center min-h-screen flex-wrap-reverse lg:flex-nowrap gap-10 lg:gap-0 lg:mt-10 lg:my-0  my-20">
+    <section className="w-[95%] sm:w-[90%] md:w-[80%]  lg:w-[75%] mx-auto flex justify-center items-center min-h-screen flex-wrap-reverse lg:flex-nowrap gap-8 lg:gap-0 lg:mt-10 lg:my-0  my-20">
       <div className="flex flex-col gap-6">
         <Title first="Health" middle="Insurance." last="Simplified" />
-        <SubText Style="px-4 lg:px-0 text-lg">
+        <SubText Style="px-4 lg:px-0 pr-0 lg:pr-8 text-lg">
           Insurance is a vital part of any business.
           <br /> Get industry-focused insurance solutions that are best-suited
           for your teams and business.
